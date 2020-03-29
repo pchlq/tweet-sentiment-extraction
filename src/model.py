1a549c877f954e8c4f69ebf3121dbfb57d73d253
pchlq <peskovps@gmail.com>
2020-03-29 13:01:19 +0300

    base model done

---
+++ src/model.py
@@ -0,0 +1,28 @@
+import config
+import transformers
+import torch.nn as nn
+
+
+class BERTBaseUncased(nn.Module):
+    def __init__(self):
+        super(BERTBaseUncased, self).__init__()
+        self.bert = transformers.BertModel.from_pretrained(config.BERT_PATH)
+        self.l0 = nn.Linear(768, 2)
+    
+    def forward(self, ids, mask, token_type_ids, sentiment=None):
+        # ADD SENTIMENT..
+        sequence_output, pooled_output = self.bert(
+            ids, 
+            attention_mask=mask,
+            token_type_ids=token_type_ids
+        )
+        # (batch_size, num_tokens, 768)
+        logits = self.l0(sequence_output)
+        # (batch_size, num_tokens, 2)
+        # (batch_size, num_tokens, 1), (batch_size, num_tokens, 1)
+        start_logits, end_logits = logits.split(1, dim=-1)
+        start_logits = start_logits.squeeze(-1)
+        end_logits = end_logits.squeeze(-1)
+        # (batch_size, num_tokens), (batch_size, num_tokens)
+
+        return start_logits, end_logits
